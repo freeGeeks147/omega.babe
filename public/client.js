1ed42fa8c62204874ac6f77271c69326db3325e4
freeGeeks147 <anshuanamika44@gmail.com>
2025-06-19 23:59:17 +0530

    j

--- public/client.js
+++ public/client.js
@@ -3,15 +3,25 @@ import * as mediasoupClient from 'https://unpkg.com/mediasoup-client@3/lib/index
 
 const socket = io();
 let device, producerTransport, consumerTransport;
+let localStream;
 const localVideo = document.getElementById('localVideo');
 const remoteVideo = document.getElementById('remoteVideo');
 
+document.getElementById('start').onclick = start;
+
 async function start() {
+  console.log('Requesting local media...');
+  try {
+    localStream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    localVideo.srcObject = localStream;
+    console.log('Local media obtained');
+  } catch (err) {
+    console.error('Failed to get local media:', err);
+    return;
+  }
   console.log('Joining room...');
   socket.emit('join');
 }
-
-document.getElementById('start').onclick = start;
 
 socket.on('waiting', () => console.log('Waiting for a peer...'));
 
@@ -25,7 +35,7 @@ socket.on('roomReady', async ({ roomId }) => {
   device = new mediasoupClient.Device();
   await device.load({ routerRtpCapabilities: rtpCapabilities });
 
-  // 3. Setup consumer transport + handler
+  // 3. Setup consumer transport
   const recvParams = await new Promise(res => socket.emit('createTransport', { roomId }, res));
   consumerTransport = device.createRecvTransport(recvParams);
   consumerTransport.on('connect', ({ dtlsParameters }, cb) => {
@@ -34,21 +44,17 @@ socket.on('roomReady', async ({ roomId }) => {
   });
 
   socket.on('newProducer', async ({ producerId }) => {
-    console.log('New producer available:', producerId);
-    try {
-      const params = await new Promise(res => socket.emit('consume', { roomId, producerId, rtpCapabilities: device.rtpCapabilities }, res));
-      if (params.error) {
-        console.warn('Consume error', params.error);
-        return;
-      }
-      const consumer = await consumerTransport.consume(params);
-      const remoteStream = new MediaStream();
-      remoteStream.addTrack(consumer.track);
-      remoteVideo.srcObject = remoteStream;
-      console.log('Rendering remote stream');
-    } catch (err) {
-      console.error('Error consuming:', err);
+    console.log('New producer:', producerId);
+    const params = await new Promise(res => socket.emit('consume', { roomId, producerId, rtpCapabilities: device.rtpCapabilities }, res));
+    if (params.error) {
+      console.warn('Cannot consume:', params.error);
+      return;
     }
+    const consumer = await consumerTransport.consume(params);
+    const remoteStream = new MediaStream();
+    remoteStream.addTrack(consumer.track);
+    remoteVideo.srcObject = remoteStream;
+    console.log('Remote stream rendered');
   });
 
   // 4. Setup producer transport
@@ -63,16 +69,9 @@ socket.on('roomReady', async ({ roomId }) => {
     socket.emit('produce', { roomId, kind, rtpParameters }, cb);
   });
 
-  // 5. Get local media and produce
-  try {
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-    console.log('Got local stream');
-    localVideo.srcObject = stream;
-    stream.getTracks().forEach(track => {
-      console.log('Producing track:', track.kind);
-      producerTransport.produce({ track });
-    });
-  } catch (err) {
-    console.error('Could not get user media:', err);
-  }
+  // 5. Produce tracks from localStream
+  localStream.getTracks().forEach(track => {
+    console.log('Producing track:', track.kind);
+    producerTransport.produce({ track });
+  });
 });
